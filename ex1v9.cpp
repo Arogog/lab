87ad3af21c41329b11cc8bab1ab6772fa66f07bd
Arogog <43525615+Arogog@users.noreply.github.com>
2018-12-27 12:44:00 +0300

    Update ex1v9.cpp

--- ex1v9.cpp
+++ ex1v9.cpp
@@ -1,6 +1,6 @@
-#include "pch.h"
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 int main() {
@@ -8,12 +8,15 @@ int main() {
 	cout << "Enter a \n";
 	cin >> a;
 	cout << "Enter b \n";
-	cin	>> b;
+	cin >> b;
 
 	z1 = (cos(a) - cos(b))*(cos(a) - cos(b)) - (sin(a) - sin(b))*(sin(a) - sin(b));
 	z2 = -4 * sin((a - b) / 2)*sin((a - b) / 2)*cos(a + b);
-	cout <<"z1 = " << z1 << endl<<"z2 = " << z2 <<endl;
+	cout << "z1 = " << z1 << endl << "z2 = " << z2 << endl;
 	if (z1 == z2) { cout << "z1 equally z2"; }
 	else { cout << "z1 not equally z2"; }
+	cin.get();
+	cin.get();
 	return 0;
+
 }
